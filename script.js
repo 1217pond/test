6b18fd828c2fd33ec593fd7ecfe602ebdb1d6718
1217pond <83156814+1217pond@users.noreply.github.com>
2022-03-13 09:09:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function run(){
-  let a = CryptoJS.AES.encrypt(SrcText.value, KeyText.value);
-  console.log('Crypto :' + a);
+  let a = CryptoJS.AES.encrypt(SrcText.value, KeyText.value, { iv: 0, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 }).toString();
+  console.log(a);
 }
 
 encryption.addEventListener("click",run);
